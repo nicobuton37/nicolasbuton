333bb099d8b20064a1384f9e28d003348d6c17e2
nicobuton37 <nicobuton370@gmail.com>
2017-07-13 14:28:40 +0200

    rename works and animate talents

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 
 $(document).ready(function() {
   console.log("ready");
-  $("#listBlock").hide();
+  $("#listBlock, #worksBlock").hide();
   $("#titlePolice, #smallTitle").hide();
   $("#titlePolice").fadeIn(2500);
   $("#smallTitle").fadeIn(8000);
@@ -9,7 +9,41 @@ $(document).ready(function() {
 
   $("#menu").click(function(){
         $("#listBlock").animate({
-            height: 'toggle'
+            height: 'toggle',
+
         });
     });
+    $("#talents").click(function(){
+      callAjaxTalents()
+      $("#listBlock").hide(1000);
+    });
+
+    $("#worksBlock").click(function(){
+      $("#worksBlock, #listBlock").hide(2000);
+    });
+
 });
+
+function callAjaxTalents(){
+  $.ajax({
+    url: 'talents.html',
+    method: 'GET',
+  })
+  .done(function(data) {
+    console.log("success");
+
+    $("#worksBlock").html(data).show().animate({
+          left: '250px',
+          opacity: '0.6',
+          height: '400px',
+          width: '400px'
+    });
+  })
+  .fail(function() {
+    console.log("error");
+  })
+  .always(function() {
+    console.log("complete");
+  });
+
+}
